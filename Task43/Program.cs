fc09969b4e1b39b384ee29097fa944e2ce309be1
zhyk <zhyk@list.ru>
2023-11-17 10:27:53 +0300

    Задачи № 43 и 45

---
+++ Task43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double InputNumber(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double k1 = InputNumber("Введите коэффициент k1: ");
+double b1 = InputNumber("Введите коэффициент b1: ");
+double k2 = InputNumber("Введите коэффициент k2: ");
+double b2 = InputNumber("Введите коэффициент b2: ");
+double x = 0;
+double y = 0;
+
+if ((k1 == k2) & (b1 != b2))
+    {
+        Console.WriteLine("Прямые не пересекаются");
+    }
+else
+{
+    if ((k1 == k2) & (b1 == b2))
+    {
+        Console.WriteLine("Прямые совпадают");
+    }
+    else
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k1 * x + b1;  
+        Console.WriteLine($"Координаты точки пересечения: ({x}, {y})");
+    }
+}
+
+
+
---
+++ Task45/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа создает кпию заданного массиа.
+
+int[] FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,10);
+    }
+    return arr;
+}
+
+int GetNumber(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CopyArray(int[] soursearr)
+{
+    int[] newarray = new int[soursearr.Length];
+    for (int i = 0; i < soursearr.Length; i++)
+    {
+        newarray[i] = soursearr[i];
+    }
+    return newarray;
+}
+
+int[] array = new int[GetNumber("Введите кол-во элементов массива: ")];
+array = FillArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int[] arraycopy = new int[array.Length];
+arraycopy = CopyArray(array);
+Console.WriteLine($"[{string.Join(", ", arraycopy)}]");
+
+
+
+Console.WriteLine("Hello, World!");
